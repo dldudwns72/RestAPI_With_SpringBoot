4ccf0a0d6aa9ed56f5b5da6e4be5eb6028a5d03d
LeeYoungJun <dldudwns72@gmail.com>
2021-01-25 11:39:41 +0900

    Test 실패 결과 이유 파악

--- demo-inflean-rest-api/src/test/java/me/whiteship/demoinfleanrestapi/eventTests/EventControllerTests.java
+++ demo-inflean-rest-api/src/test/java/me/whiteship/demoinfleanrestapi/eventTests/EventControllerTests.java
@@ -1,16 +1,26 @@
 package me.whiteship.demoinfleanrestapi.eventTests;
 
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Test;
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import me.whiteship.demoinfleanrestapi.events.Event;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -20,12 +30,34 @@ public class EventControllerTests {
 	@Autowired
 	MockMvc mockMvc;
 	
+	// Jackson 라이브러리가 설정 되어있다면 자동으로 등록 가능
+	@Autowired
+	ObjectMapper objectMapper;
+	
+	// 201을 던져줘야 하는데 404를 던져준다.
 	@Test
 	public void createEvent() throws Exception {
+		Event event = Event.builder()
+							.name("Spring")
+							.description("REST API")
+							.beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 23, 14, 21))
+							.closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 24, 14, 21))
+							.beginEventDateTime(LocalDateTime.of(2018, 11, 25, 14, 21))
+							.basePrice(100)
+							.maxPrice(200)
+							.limitOfEnrollment(100)
+							.location("강남역 D2 스타텀 팩토리")
+							.build();
+									
 		mockMvc.perform(post("/api/events/")
 				.contentType(MediaType.APPLICATION_JSON_UTF8) // contentType 설정 
 				.accept(MediaTypes.HAL_JSON)
-				).andExpect(status().isCreated());
+				.content(objectMapper.writeValueAsString(event))) // event 객체를 받아서 JSON화(Serializaion)을 해준다.
+		.andDo(print())
+		.andExpect(status().isCreated()) // JSON 예상 응답 201
+		.andExpect((ResultMatcher) jsonPath("id").exists())
+		
+		;
 		
 	}
 	
