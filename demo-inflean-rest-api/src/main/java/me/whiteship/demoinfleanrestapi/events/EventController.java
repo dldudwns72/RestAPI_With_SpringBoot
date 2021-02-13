a9a7fd21f4aed180954371a6eb2228fe257aab0b
LeeYoungJun <dldudwns72@gmail.com>
2021-02-13 16:21:43 +0900

    빈값 및 잘못된 값 입력 후 테스트 코드 작성

--- demo-inflean-rest-api/src/main/java/me/whiteship/demoinfleanrestapi/events/EventController.java
+++ demo-inflean-rest-api/src/main/java/me/whiteship/demoinfleanrestapi/events/EventController.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,8 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.net.URI;
 
+import javax.validation.Valid;
+
 
 @Controller
 @RequestMapping(value = "/api/events/", produces = MediaTypes.HAL_JSON_VALUE) //,produces = MediaTypes.HAL_JSON 
@@ -24,22 +28,33 @@ public class EventController {
 	
 	private final ModelMapper modelMapper;
 	
-	public EventController(EventRepository eventRepository, ModelMapper modelMapper) {
+	private EventValidator eventValidator;
+	
+	public EventController(EventRepository eventRepository, ModelMapper modelMapper,EventValidator eventValidator) {
 		// 생성자가 하나만 있고 생성자로 받아올 파라미터가 이미 Bean으로 등록되어있다면 Autowired 생략 가능 Spring 4.X 이상부터 
 		this.eventRepository = eventRepository;
 		this.modelMapper = modelMapper;
+		this.eventValidator = eventValidator;
 	}
 	
 	// 상위 코드는 @Autowired EventRepository eventRepository 와 동일한 코드
 	
+	// @Valid 설정 시 설정한 객체에 유효성 검사를 설정 할 수 있다.
 	@PostMapping
-	public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+	public ResponseEntity createEvent(@RequestBody  @Valid EventDto eventDto , Errors errors) {
 //		URI createdUri = linkTo(methodOn(EventController.class).createEvent()).slash("{id}").toUri();
 //		return ResponseEntity.created(createdUri).build();
 		// 상위 코드 201 응답을 만들기 위한 방법
 		
+		if(errors.hasErrors()) {
+			return ResponseEntity.badRequest().build();
+		}
+		
+		eventValidator.validate(eventDto, errors);
+		
 		// 기존 대로라면 Event event = Event.builder()안에 값 다 설정해 주고 Dto에 값을 넣어줘야 하지만 ModelMapper 사용으로 DTO를 도메인 객체로 값 을 복사해준다. maven 설정 필요
 		Event event = modelMapper.map(eventDto, Event.class);
+		
 		
 		Event newEvent = this.eventRepository.save(event);
 		URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
